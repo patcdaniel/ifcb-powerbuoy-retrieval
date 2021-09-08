311819aab397c97fe18932c5cbd95cebd4f361c9
Patrick Daniel <pdaniel@mbari.org>
2021-09-08 16:41:25 +0000

    Updated Transfer.py to catch droped files.

--- transfer.py
+++ transfer.py
@@ -4,7 +4,7 @@ Organize IFCB files into a standard structure:
 /CA-IFCB-161/2021/D20210815/{FILES} -
 """
 
-import os, glob, re, shutil
+import os, glob, re, shutil, subprocess
 
 # GLOBALS
 DATA_PATH = "/opt/ifcb-data/power-buoy-deployment/"
@@ -17,29 +17,36 @@ def transfer_files_directories():
     Assume that there are .hdr, .roi, and .adc files for each filename
     Example structure /CA-IFCB-161/2021/D20210718/
     """
-    for fname in sorted(glob.glob(DATA_PATH+"*.hdr")):
-        pattern = r"(D(\d\d\d\d)(\d\d\d\d))"
-        base_name = os.path.basename(fname)
-        date_str = re.findall(pattern, base_name)
 
-        if bool(date_str):
-            year = date_str[0][1]
-            unique_day = date_str[0][0]
+    formatted_data = [os.path.basename(f) for f in sorted(glob.glob(DES_Path+"/*/*/*.*"))] # All files already transfered
 
-            if not os.path.isdir(os.path.join(DES_Path,year)):
-                oldmask = os.umask(000)
-                os.makedirs(os.path.join(DES_Path,year,unique_day), 0o777)
-                os.umask(oldmask)
-            
-            else:
-                if not os.path.isdir(os.path.join(DES_Path,year,unique_day)):
+    for fname in sorted(glob.glob(DATA_PATH+"*.*")):
+        if not fname in formatted_data:
+            pattern = r"(D(\d\d\d\d)(\d\d\d\d))"
+            base_name = os.path.basename(fname)
+            date_str = re.findall(pattern, base_name)
+
+            if bool(date_str):
+                year = date_str[0][1]
+                unique_day = date_str[0][0]
+
+                if not os.path.isdir(os.path.join(DES_Path,year)):
                     oldmask = os.umask(000)
-                    os.mkdir(os.path.join(DES_Path,year,unique_day), 0o775)
+                    os.makedirs(os.path.join(DES_Path,year,unique_day), 0o777)
                     os.umask(oldmask)
-            
-            shutil.copy(fname, os.path.join(DES_Path,year,unique_day,base_name))
-            shutil.copy(os.path.join(DATA_PATH, base_name.split('.')[0]) + ".roi", os.path.join(DES_Path,year,unique_day,base_name.split('.')[0] + ".roi"))
-            shutil.copy(os.path.join(DATA_PATH ,base_name.split('.')[0]) + ".adc", os.path.join(DES_Path,year,unique_day,base_name.split('.')[0] + ".adc"))
+                
+                else:
+                    if not os.path.isdir(os.path.join(DES_Path,year,unique_day)):
+                        oldmask = os.umask(000)
+                        os.mkdir(os.path.join(DES_Path,year,unique_day), 0o775)
+                        os.umask(oldmask)
+                try:
+                    shutil.copy(fname, os.path.join(DES_Path,year,unique_day,base_name))
+
+                except OSError as e:
+                    # File didn't transfer completely. Try to grab it again.
+                    subprocess.run(["scp",'-l','1000',"ifcb@buoy-ifcb.shore.mbari.org:/mnt/data/ifcbdata/"+fname, DATA_PATH]) #MegaB/Sec
+                    shutil.copy(os.path.join(DATA_PATH ,fname), os.path.join(DES_Path,year,unique_day,fname))                    
 
 
 if __name__ == "__main__":
